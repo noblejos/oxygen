932620e5f2ec9cd22b122299600e08c088d80c95
noblejos <98827185+noblejos@users.noreply.github.com>
2022-04-05 07:54:49 +0100

    oxygen

---
+++ app.js
@@ -0,0 +1,17 @@
+let burgerBtn=document.querySelector(".burger");
+let navLinks = document.querySelector(".nav-links");
+
+burgerBtn.addEventListener("click", function(){
+    navLinks.classList.toggle("active");
+    burgerBtn.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
+
